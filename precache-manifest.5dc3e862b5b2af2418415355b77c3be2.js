ec151bbc75455adb40ed2bb93745eae9700e7f22
plkpiotr <plk.piotr.pp@gmail.com>
2019-11-16 19:23:19 +0100

    Updates

---
+++ precache-manifest.5dc3e862b5b2af2418415355b77c3be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f6d69ae09908588f0224d597f0deb5",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "40e06186c3ea47cd7078",
+    "url": "/photo-gallery/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d058cfacd0fbc38e4c47",
+    "url": "/photo-gallery/static/js/2.3c3f1856.chunk.js"
+  },
+  {
+    "revision": "40e06186c3ea47cd7078",
+    "url": "/photo-gallery/static/js/main.9f1a12f1.chunk.js"
+  },
+  {
+    "revision": "089fd024c8126aa02c6d",
+    "url": "/photo-gallery/static/js/runtime-main.6dda176b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/photo-gallery/static/media/logo.25bf045c.svg"
+  }
+]);
