1da9b5dece6e8eb63a3336027549edc8e850da75
plkpiotr <plk.piotr.pp@gmail.com>
2019-11-17 00:32:48 +0100

    Add remaining files

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,15 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans&display=swap&subset=latin-ext');
+  
+  body {
+    font-family: 'Noto Sans', sans-serif;
+    margin: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default GlobalStyle;
